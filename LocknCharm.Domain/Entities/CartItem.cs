a628e638609d5baccffaedc5916b44869e8d32b5
QuocHung <hungnguyen@maple.com.vn>
2025-06-30 20:04:00 +0700

    refactor - adjust db to using TPT

--- LocknCharm.Domain/Entities/CartItem.cs
+++ LocknCharm.Domain/Entities/CartItem.cs
@@ -4,12 +4,14 @@ namespace LocknCharm.Domain.Entities
 {
     public class CartItem : BaseEntity
     {
-        public Guid? PreMadeKeychainId { get; set; }
-        public PreMadeKeychain? PreMadeKeychain { get; set; } = null!;
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; } = null!;
 
         public Guid CartID { get; set; }
         public Cart Cart { get; set; } = null!;
-        public decimal Price { get; set; }
+        public decimal Price { get; set; } = 0.0m;
         public int Quantity { get; set; } = 1;
+        public decimal TotalPrice { get; set; } = 0.0m;
+
     }
 }
---
+++ LocknCharm.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using LocknCharm.Domain.Enums;
+
+namespace LocknCharm.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; } = new Guid();
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = null!; //khi user tạo móc khóa custom, sẽ upload ảnh lên firebase storage và lấy link ảnh để lưu vào đây
+        public int Stock { get; set; }
+        public ProductType? Type { get; set; } = null;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; }
+    }
+}
